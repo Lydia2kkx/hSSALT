fc59529475d7256f090492c1880abfabe44dfc76
TheCq96 <104197586+TheCq96@users.noreply.github.com>
2023-11-24 16:09:25 +0100

    Bugfixing
    -fixed incorrect variable names
    -added input variable theta1
    -corrected data selection error in calculating jackknife estimate

--- R/CIbs_hSSALT.R
+++ R/CIbs_hSSALT.R
@@ -1,16 +1,17 @@
 
-CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B, theta_21,
-            theta_22, p, maxit, tol, language, parallel, ncores){
-  
+CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B, theta1, theta_21,
+                       theta_22, p, maxit, tol, language, parallel, ncores){
+
   same_threshold <- 0.05
   p_threshold <- 0.01
   p_adjust <- TRUE ##adjust p with same results to 0.5
   data_adjust <- TRUE ##discard same results of theta21 and theta22
-  
+  mle1<-theta1
+
   if (monitoring=="Interval"){
-    
-    
-    B <- 1000
+
+
+    # B <- 1000
     j <- 1  ### iteration counter
     iter <- 1
     Results <- list()
@@ -25,26 +26,26 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
         T1 <- stl_sort(T1_simu[T1_simu <= tau[1]])
         n1j <- table_factor_cpp(cut(T1, breaks = seq(0, tau[1], delta)))
         theta1_hat <- uniroot(func_theta1, c(1, 1000), extendInt = "yes", n1j = n1j, n1 = n1)$root
-        
+
         #####Second stress level
         #####Simulate the mixture
-        n21 <- rbinom(1, (n-n1), p1_em)
-        T21 <- stl_sort(-theta21_em*log(1-runif(n21)) + tau[1])
+        n21 <- rbinom(1, (n-n1), p)
+        T21 <- stl_sort(-theta21*log(1-runif(n21)) + tau[1])
         n22 <- n-n1-n21
-        T22 <- stl_sort(-theta22_em*log(1-runif(n22)) + tau[1])
+        T22 <- stl_sort(-theta22*log(1-runif(n22)) + tau[1])
         T2 <- stl_sort(c(T21, T22))
         n2 <- sum(T2 < tau[2])
         n2j <- table_factor_cpp(cut(T2[T2 < tau[2]], breaks = seq(tau[1], tau[2], delta)))
         data <- c(rep(j-1, n2j), rep(q2, n-n1-n2))
         d <- c(rep(1, n2), rep(0, n-n1-n2))
         q2 <- (tau[2]-tau[1])/delta
-        
+
         parameter_starts <- data.frame(omega1=p, theta21=theta21, theta22)
-        
+
         if(language=="CPP"){
           if(nrow(parameter_starts)==1){
-            EM_mle <- EM_algorithm_interval_arma(ind =1, data = data, N = maxit, 
-                                                      delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol) 
+            EM_mle <- EM_algorithm_interval_arma(ind =1, data = data, N = maxit,
+                                                 delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
           }else if (parallel==T){
             cl <- parallel::makeCluster(getOption("cl.cores", ncores))
             EM_mle <- parLapply(cl, 1:nrow(parameter_starts), EM_algorithm_interval_arma, data = data_starts, N = maxit,
@@ -54,37 +55,37 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
             EM_mle <- lapply(1:nrow(parameter_starts), EM_algorithm_interval_arma, data = data_starts, N = maxit,
                              delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
           }
-          
+
         }else{
-          
+
           if (nrow(parameter_starts)==1){
-            EM_mle <- EM_algorithm_interval(ind =1, data = data_starts, N = maxit, 
+            EM_mle <- EM_algorithm_interval(ind =1, data = data_starts, N = maxit,
                                             delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
-            
+
           }else if(parallel == TRUE){
-              cl <- parallel::makeCluster(getOption("cl.cores", ncores))
-              EM_mle <- parLapply(cl, 1:nrow(parameter_starts), EM_algorithm_interval, data = data_starts, N = maxit,
-                                  delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
-              parallel::stopCluster(cl)
-              
-            }else {
-              
-              EM_mle <- lapply(1:nrow(parameter_starts), EM_algorithm_interval, data = data_starts, N = maxit,
-                               delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
-            }
-        #####Apply the EM algorithm
-        # model_list <- lapply(1:nrow(parameter_starts), EM_algorithm_interval, data = data, delta = delta) 
+            cl <- parallel::makeCluster(getOption("cl.cores", ncores))
+            EM_mle <- parLapply(cl, 1:nrow(parameter_starts), EM_algorithm_interval, data = data_starts, N = maxit,
+                                delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
+            parallel::stopCluster(cl)
+
+          }else {
+
+            EM_mle <- lapply(1:nrow(parameter_starts), EM_algorithm_interval, data = data_starts, N = maxit,
+                             delta = delta, d=d, parameter_starts=parameter_starts, q2=q2, tol = tol)
+          }
+          #####Apply the EM algorithm
+          # model_list <- lapply(1:nrow(parameter_starts), EM_algorithm_interval, data = data, delta = delta)
         }
         Estimate_df <- do.call("rbind.data.frame", EM_mle)
-        
+
         ###Add some variables to the result data frame
         Estimate_df$theta1 <- theta1_hat
         Estimate_df$n2 <- n2
         ### If the estimated theta22 is larger than mle1, invalid result, set prob1 (p) to NA
         Estimate_df$prob1 <- ifelse(Estimate_df$theta22 >= theta1_hat, NA, Estimate_df$prob1)
-        ### If the estimated prob1 is too small (< 0.01) or too large (> 0.99), or the estimated theta21 and 
-        ### theta22 are too close (less than 1% of theta22), homogeneous result 
-        Estimate_df$info <- ifelse((Estimate_df$prob1 < p_threshold) | (Estimate_df$prob2 < p_threshold) | (Estimate_df$theta22 - Estimate_df$theta21 <= same_threshold*Estimate_df$theta22), 
+        ### If the estimated prob1 is too small (< 0.01) or too large (> 0.99), or the estimated theta21 and
+        ### theta22 are too close (less than 1% of theta22), homogeneous result
+        Estimate_df$info <- ifelse((Estimate_df$prob1 < p_threshold) | (Estimate_df$prob2 < p_threshold) | (Estimate_df$theta22 - Estimate_df$theta21 <= same_threshold*Estimate_df$theta22),
                                    "homo", ifelse(is.na(Estimate_df$prob1), NA, "heter"))
         Estimate_df$mle2_classi <- uniroot(func_theta2, c(1, 1000), extendInt = "yes", n2j = n2j, n1 = n1, n2 = n2)$root
         if(p_adjust){
@@ -93,7 +94,7 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
         }
         if(data_adjust){
           ### if homogeneous result, set prob1 to NA since we only consider the heterogeneous result
-          Estimate_df$prob1[Estimate_df$info == "homo"] <- NA 
+          Estimate_df$prob1[Estimate_df$info == "homo"] <- NA
         }
         if(all(is.na(Estimate_df$prob1))){ ###If all NAs in prob1, next iteration
           #seed <- seed + 1
@@ -107,28 +108,28 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
       iter <- iter + 1
       j <- j + 1 ### increase the iteration counter
     }
-    
+
     ###### Omit the NA results in each sublist. For some initial points, the results might be invalid
     Results <- lapply(Results, na.omit)
     ###### Find the estimated parameters with the maximal log likelihood
     max_loglik <- lapply(Results, find_max)
     max_loglik_df <- do.call("rbind.data.frame", max_loglik)
-    
+
     ####Find the maximum if the initial values are vectors
     #### Otherwise, it is very easy since find_max() is useless.
     #### Boostrap Percentile CIs easily take the corresponding quantiles for each parameter
     bootstrap_distri <- cbind.data.frame(i = 1:nrow(max_loglik_df), max_loglik_df)
     ### alpha is the significant level, take theta1 as an example
     # quantile(bootstrap_distri$theta1, c(alpha/2, 1-alpha/2))
-    
+
   }else{
-    
+
     ########## Continuous ##########
-    
+
     if(censoring==2){
       tau <- c(tau, data[r])
     }
-    
+
     j <- 1
     iter <- 1 ## iteration counter (how many times we need to obtain B times valid results)
     Results <- list()
@@ -142,75 +143,81 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
       }else{
         T1 <- stl_sort(T1_simu[T1_simu <= tau[1]])
         theta1_hat <- sum(T1, (n-n1)*tau[1]) / n1
-        n21 <- rbinom(1, (n-n1), p1_em)
-        T21 <- stl_sort(-theta21_em*log(1-runif(n21)) + tau[1])
+        n21 <- rbinom(1, (n-n1), p)
+        T21 <- stl_sort(-theta21*log(1-runif(n21)) + tau[1])
         n22 <- n-n1-n21
-        T22 <- stl_sort(-theta22_em*log(1-runif(n22)) + tau[1])
+        T22 <- stl_sort(-theta22*log(1-runif(n22)) + tau[1])
         T2 <- stl_sort(c(T21, T22))
         n2 <- sum(T2 < tau[2])
         d <- c(rep(1, n2), rep(0, n-n1-n2))
         t22 <- apply(cbind(T2, tau[2]), 1, min) # observed or censored data
-        
-        
-        
+
+
+
         # model_list <- lapply(1:nrow(parameter_starts), EM_algorithm_censored, data = t22 - tau[1])
-        
-        
+
+
         parameter_starts <- data.frame(omega1=p, theta21=theta21, theta22)
-        
-        
+
+
         if(language == "CPP") {
-          
+
           if (dim(parameter_starts)[1]==1){
             EM_mle <- EM_algorithm_censored_arma(ind =1, data=t22-tau[1], d=d, N=maxit, parameter_starts = parameter_starts, tol = tol)
-          
+            Estimate_df <- EM_mle
+
           }else if(parallel == TRUE){
             cl <- parallel::makeCluster(getOption("cl.cores", ncores))
             EM_mle <- parLapply(cl, 1:nrow(parameter_starts), EM_algorithm_censored_arma, data = t22 - tau[1], d=d, N=maxit,
-                               parameter_starts = parameter_starts, tol = tol)
+                                parameter_starts = parameter_starts, tol = tol)
             parallel::stopCluster(cl)
-              
+            Estimate_df <- do.call("rbind.data.frame", EM_mle)
+
           }else{
             EM_mle <- lapply(1:nrow(parameter_starts), EM_algorithm_censored_arma, data = t22 - tau[1], d=d, N=maxit,
-                               parameter_starts = parameter_starts, tol = tol)
-            }
-            
-          
+                             parameter_starts = parameter_starts, tol = tol)
+            Estimate_df <- do.call("rbind.data.frame", EM_mle)
+          }
+
+
         }else{
-          
+
           if (dim(parameter_starts)[1]==1){
             EM_mle <- EM_algorithm_censored(ind =1, data=t22-tau[1], d=d, N=maxit, parameter_starts = parameter_starts, tol)
-     
+            Estimate_df <- EM_mle
+
           }else if(parallel == TRUE){
             cl <- parallel::makeCluster(getOption("cl.cores", ncores))
             EM_mle <- parLapply(cl, 1:nrow(parameter_starts), EM_algorithm_censored, data = t22 - tau[1], d=d, N=maxit,
-                               parameter_starts = parameter_starts, tol)
+                                parameter_starts = parameter_starts, tol)
             parallel::stopCluster(cl)
-              
+            Estimate_df <- do.call("rbind.data.frame", EM_mle)
+
           }else{
             EM_mle <- lapply(1:nrow(parameter_starts), EM_algorithm_censored, data = t22 - tau[1], d=d, N=maxit,
-                               parameter_starts = parameter_starts, tol)
+                             parameter_starts = parameter_starts, tol)
+            Estimate_df <- do.call("rbind.data.frame", EM_mle)
           }
-      
-        }  
-          
-        
-        Estimate_df <- do.call("rbind.data.frame", EM_mle)
-        
-        
+
+        }
+
+
+
+
+
         Estimate_df$theta1 <- theta1_hat
         Estimate_df$n2 <- n2
-        Estimate_df$pi1 <- ifelse(Estimate_df$theta22 >= theta1_hat, NA, Estimate_df$pi1)
-        Estimate_df$info <- ifelse((Estimate_df$pi1 < p_threshold) | (Estimate_df$pi2 < p_threshold) | (Estimate_df$theta22 - Estimate_df$theta21 <= same_threshold*Estimate_df$theta22), 
-                                   "homo", ifelse(is.na(Estimate_df$pi1), NA, "heter"))
+        Estimate_df$p1 <- ifelse(Estimate_df$theta22 >= theta1_hat, NA, Estimate_df$p1)
+        Estimate_df$info <- ifelse((Estimate_df$p1 < p_threshold) | (Estimate_df$p2 < p_threshold) | (Estimate_df$theta22 - Estimate_df$theta21 <= same_threshold*Estimate_df$theta22),
+                                   "homo", ifelse(is.na(Estimate_df$p1), NA, "heter"))
         Estimate_df$mle2_classi <- (sum(T2[1:n2] - tau[1]) + (n - n1 - n2) * (tau[2] - tau[1]))/ n2
         if(p_adjust){
-          Estimate_df$pi1[Estimate_df$info == "homo"] <- Estimate_df$pi2[Estimate_df$info == "homo"] <- 0.5
+          Estimate_df$p1[Estimate_df$info == "homo"] <- Estimate_df$p2[Estimate_df$info == "homo"] <- 0.5
         }
         if(data_adjust){
-          Estimate_df$pi1[Estimate_df$info == "homo"] <- NA 
+          Estimate_df$p1[Estimate_df$info == "homo"] <- NA
         }
-        if(all(is.na(Estimate_df$pi1))){
+        if(all(is.na(Estimate_df$p1))){
           #seed <- seed + 1
           iter <- iter + 1
           next
@@ -222,10 +229,10 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
       iter <- iter + 1
       j <- j + 1 ### NN # increase the iteration counter
     }
-    
+
     ####Omit NAs in the result list
     Results_omit <- lapply(Results, na.omit)
-    
+
     ####Find the maximum if the initial values are vectors
     # find_max <- function(x){
     #   index <- which.max(x$loglik)
@@ -233,24 +240,23 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
     #   row.names(y) <- NULL
     #   return(y)
     # }
-    
+
     max_loglik <- lapply(Results_omit, find_max)
     max_loglik_df <- do.call("rbind.data.frame", max_loglik)
-    
+
     #### Otherwise, it is very easy since find_max() is useless.
     #### Boostrap Percentile CIs easily take the corresponding quantiles for each parameter
     bootstrap_distri <- cbind.data.frame(i = 1:nrow(max_loglik_df), max_loglik_df)
     ### alpha is the significant level, take theta1 as an example
     # quantile(bootstrap_distri$theta1, c(alpha/2, 1-alpha/2))
-    
-    
+
+
   }
-  
-  return(as.list(CI_theta1 = quantile(bootstrap_distri$theta1, c(alpha/2, 1-alpha/2)), 
-               CI_theta21 = quantile(bootstrap_distri$theta21, c(alpha/2, 1-alpha/2)),
-               CI_theta22 = quantile(bootstrap_distri$theta22, c(alpha/2, 1-alpha/2)),
-               CI_p = quantile(bootstrap_distri$p, c(alpha/2, 1-alpha/2))
-))
-  
+
+  return(list(CI_theta1 = quantile(bootstrap_distri$theta1, c(alpha/2, 1-alpha/2)),
+              CI_theta21 = quantile(bootstrap_distri$theta21, c(alpha/2, 1-alpha/2)),
+              CI_theta22 = quantile(bootstrap_distri$theta22, c(alpha/2, 1-alpha/2)),
+              CI_p = quantile(bootstrap_distri$p, c(alpha/2, 1-alpha/2))
+  ))
+
 }
-  
