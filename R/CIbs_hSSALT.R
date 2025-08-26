871adea429f535349dff137b9b98283bf49712c6
Yao Lu <yao.lu1@rwth-aachen.de>
2025-08-26 22:21:34 +0200

    better format

--- R/CIbs_hSSALT.R
+++ R/CIbs_hSSALT.R
@@ -3,8 +3,10 @@ CIbs_hSSALT <-function(data, n, censoring, tau , r, monitoring, delta, alpha, B,
                        theta22, p, maxit, tol, language, parallel, ncores,grid){
 
     
-  bootstrap_distri <- bootstrap_distribution(data, n,monitoring=monitoring,theta1=theta1,theta21=theta21,
-                                                theta22=theta22,p=p,censoring, tau , r, B, delta=delta,maxit, tol, language, parallel, ncores,grid)
+  bootstrap_distri <- bootstrap_distribution(data, n, monitoring = monitoring, theta1 = theta1,
+                                             theta21 = theta21, theta22 = theta22, p = p, 
+                                             censoring, tau, r, B, delta = delta, maxit, tol, 
+                                             language, parallel, ncores, grid)
     
   
   lower_bound <- c(quantile(bootstrap_distri$theta1, alpha/2)[[1]],quantile(bootstrap_distri$theta21, alpha/2)[[1]],quantile(bootstrap_distri$theta22, alpha/2)[[1]],quantile(bootstrap_distri$p1, alpha/2)[[1]])
