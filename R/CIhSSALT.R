28805972458111cd15f6ced10fc799cee26abbe9
avnermerims <avner.merims@rwth-aachen.de>
2025-11-12 22:05:30 +0100

    Update CIhSSALT.R

--- R/CIhSSALT.R
+++ R/CIhSSALT.R
@@ -35,8 +35,9 @@
 CIhSSALT <- function(data, n, MLEhSSALT_Obj, censoring = 1, tau, r=NULL, monitoring = "continuous",
                      delta = NULL, CImethod = "asymptotic", alpha = 0.05, B = 1000, maxit = 1000, 
                      tol=1e-8, language = "CPP"){
-  #Avner: For what cases is sorting the data necessary? This causes issues with interval monitoring.
-  #data <- sort(data)
+  if (monitoring == "continuous") {
+    data <- sort(data) 
+  }
   theta1 <- MLEhSSALT_Obj$mle$theta1
   theta21 <- MLEhSSALT_Obj$mle$theta21
   theta22 <- MLEhSSALT_Obj$mle$theta22
