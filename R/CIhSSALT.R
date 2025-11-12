619c3ae0088189c8eca533177dea995bfa2fa6da
avnermerims <avner.merims@rwth-aachen.de>
2025-11-12 21:39:47 +0100

    Sorted data comment

--- R/CIhSSALT.R
+++ R/CIhSSALT.R
@@ -35,7 +35,8 @@
 CIhSSALT <- function(data, n, MLEhSSALT_Obj, censoring = 1, tau, r=NULL, monitoring = "continuous",
                      delta = NULL, CImethod = "asymptotic", alpha = 0.05, B = 1000, maxit = 1000, 
                      tol=1e-8, language = "CPP"){
-  data <- sort(data)
+  #Avner: For what cases is sorting the data necessary? This causes issues with interval monitoring.
+  #data <- sort(data)
   theta1 <- MLEhSSALT_Obj$mle$theta1
   theta21 <- MLEhSSALT_Obj$mle$theta21
   theta22 <- MLEhSSALT_Obj$mle$theta22
